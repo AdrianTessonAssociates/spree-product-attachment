9e83e21eb874df5723ffc110e17a7077535252e9
Dan Wentworth <dan.wentworth@gmail.com>
2010-07-13 17:56:18 +0100

    Improved layout of products hook

--- product_attachment_hooks.rb
+++ product_attachment_hooks.rb
@@ -2,6 +2,6 @@ class ProductAttachmentHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_product_tabs, "admin/shared/product_downloadables_tab"
 
-  insert_before :cart_form,  'downloadable_links'
+  insert_after :product_description,  'downloadable_links'
 
 end
